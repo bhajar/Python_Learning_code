6cb1da09304d0577f00548179ea688e5044c086f
kanishkd4 <kanishkd4@gmail.com>
2016-06-27 23:26:56 +0530

    Add files via upload

---
+++ Datacamp - Intro to python/Basics.py
@@ -0,0 +1,43 @@
+print(5 / 8)
+
+print(7 + 10)
+
+# print works differently from python 2 as it is a function in python 3
+
+# ** gives exponential; % gives remainder
+# Addition and subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication and division
+print(3 * 5)
+print(10 / 2)
+
+# Exponentiation
+print(4 ** 2)
+
+# Modulo
+print(18 % 7)
+
+# How much is your $100 worth after 7 years if invested at 10%?
+print(100 * (1.1**7)) 
+
+
+# Video on variables and types
+# Defining a variable. same height and weight as LPHW
+
+height = 1.82
+weight = 82
+BMI = weight/height**2
+BMI
+type(BMI) # types are str, bool(boolean), int, float
+
+savings = 100
+print(savings)
+
+factor = 1.1
+result = savings * factor**7
+print(result)
+
+
+
